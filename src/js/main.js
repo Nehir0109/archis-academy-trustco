1027aa3d9b7a0d5b241342921c9b2d8701eaab3d
Nehir0109 <nehir.cakilci@gmail.com>
2025-02-14 17:24:04 +0300

    [TR-07] - header section added to both index.html and contact.html files

---
+++ src/js/main.js
@@ -0,0 +1,29 @@
+/* TR-07 - HEADER SECTİON START */
+const hamburger = document.querySelector(".hamburger-menu");
+const menu = document.querySelector(".navbar-menu");
+
+hamburger.addEventListener("click", () => {
+    hamburger.classList.toggle("active");
+    menu.classList.toggle("active");
+});
+document.addEventListener("DOMContentLoaded", function() {
+    const navLinks = document.querySelectorAll(".nav-link");
+    const currentPath = window.location.pathname;
+
+    navLinks.forEach((link) => {
+        if(link.getAttribute("href") === currentPath){
+            link.classList.add("active");
+        }
+    });
+});
+
+const loginBtn = document.querySelector(".login-btn");
+const signupBtn = document.querySelector(".signup-btn");
+
+loginBtn.addEventListener("click", () => {
+    console.log("Login!");
+});
+signupBtn.addEventListener("click", () => {
+    console.log("Sign Up!");
+});
+/* TR-07 - HEADER SECTİON END */
