7d134fce77ebb43ea1248390deb2d3819a5fce1f
Özcan Sert <ozcsert0@gmail.com>
2025-02-17 12:06:55 +0300

    [TR-07] - degisiklik onerileri yapildi

--- src/js/main.js
+++ src/js/main.js
@@ -3,27 +3,32 @@ const hamburger = document.querySelector(".hamburger-menu");
 const menu = document.querySelector(".navbar-menu");
 
 hamburger.addEventListener("click", () => {
-    hamburger.classList.toggle("active");
-    menu.classList.toggle("active");
+  hamburger.classList.toggle("active");
+  menu.classList.toggle("active");
 });
-document.addEventListener("DOMContentLoaded", function() {
-    const navLinks = document.querySelectorAll(".nav-link");
-    const currentPath = window.location.pathname;
+// document.addEventListener("DOMContentLoaded", function () {
 
-    navLinks.forEach((link) => {
-        if(link.getAttribute("href") === currentPath){
-            link.classList.add("active");
-        }
-    });
-});
+const changeLinkVisual = () => {
+  const navLinks = document.querySelectorAll(".nav-link");
+  const currentPath = window.location.pathname;
+
+  navLinks.forEach((link) => {
+    if (link.getAttribute("href") === currentPath) {
+      link.classList.add("active");
+    }
+  });
+};
+// });
+
+changeLinkVisual();
 
 const loginBtn = document.querySelector(".login-btn");
 const signupBtn = document.querySelector(".signup-btn");
 
 loginBtn.addEventListener("click", () => {
-    console.log("Login!");
+  console.log("Login!");
 });
 signupBtn.addEventListener("click", () => {
-    console.log("Sign Up!");
+  console.log("Sign Up!");
 });
 /* TR-07 - HEADER SECTİON END */
